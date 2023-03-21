63d655d51f9e54d808968399fb3f6d1eaef1d16b
JulianDevz <jpachonl99@gmail.com>
2023-03-21 18:40:07 -0500

    Configuracion imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-const {src, dest, watch} = require("gulp"); //Mandamos llamar a gulp (el que instalamos en el packaje.json), el require es una forma de extraerlo // Extraemos el source y destino de gulp
+const {src, dest, watch, parallel} = require("gulp"); //Mandamos llamar a gulp (el que instalamos en el packaje.json), el require es una forma de extraerlo // Extraemos el source y destino de gulp, watch para observar cambios y parallel para ejecutar en paralelo
 
+//CSS
 const sass = require("gulp-sass")(require("sass")); //Importamos sass, con la ayuda del conector que es gulp sass
 const plumber = require("gulp-plumber"); //Extraemos la variable de plumber
+
+//IMAGEN
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
     src("src/scss/**/*.scss") // Idenfitica todas las carpetas que esten dentro de scss y todos los archivos con esa extension
@@ -13,6 +20,44 @@ function css(done){
     done(); // Callback que avisa cuando llegamos al final
 }
 
+function imagenes(done){
+    // Las imagenes las vamos a optimizar a un nivel de 3 utilizando imagemin
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpeg,jpg}')
+        .pipe( cache(imagemin(opciones) ) )
+        .pipe( dest('build/img'))
+    done();
+}
+
+function versionWebp(done){
+
+    //Calidad de las imagenes al convertir
+    const opciones = {
+        quality: 50
+    }
+
+    src('src/img/**/*.{png,jpeg,jpg}') //Entramos a todas las carpetas dentro de img y vemos todos los archivos que tengan esos formato
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img')) //almacenamos
+    done();
+}
+
+function versionAvif(done){
+
+    //Calidad de las imagenes al convertir
+    const opciones = {
+        quality: 50
+    }
+
+    src('src/img/**/*.{png,jpeg,jpg}') //Entramos a todas las carpetas dentro de img y vemos todos los archivos que tengan esos formato
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img')) //almacenamos
+    done();
+}
+
 // Cuando cambie la hoja de estilos manda a llamar la funcion css, asi tomara los cambios de forma automatica en los archivos de sass
 function dev(done){
     watch("src/scss/**/*.scss", css) //quedamos mirando todos los archivos sass para ejecutar los cambios de forma automatica
@@ -21,4 +66,7 @@ function dev(done){
 }
 
 exports.css = css; //Mandamos a llamar la funcion
-exports.dev = dev; //Llamamos la funcion que aplica el watch, esta es la que debemos ejecutar si queremos estar escuchando los cambios en el archivo de sass (ejecutariamos npx gulp dev)
+exports.imagenes = imagenes; //Mandamos a llamar la funcion
+exports.versionWebp = versionWebp; //Mandamos a llamar la funcion para convertir imagenes
+exports.versionAvif = versionAvif; //Mandamos a llamar la funcion para convertir imagenes
+exports.dev = parallel(imagenes, versionWebp, versionAvif,dev); //Llamamos la funcion que aplica el watch, esta es la que debemos ejecutar si queremos estar escuchando los cambios en el archivo de sass (ejecutariamos npx gulp dev), esto en paralelo ejecuta tambien la conversion de las imagenes a webp, y optimizar las imagenes con la funcion imagenes
