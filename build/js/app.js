33da11a34a56094c780c3a047676f261c03b984c
JulianDevz <jpachonl99@gmail.com>
2023-04-03 11:16:37 -0500

    Efecto smooth scroll

--- build/js/app.js
+++ build/js/app.js
@@ -6,6 +6,22 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function iniciarApp(){ 
     crearGaleria();
+    scrollNav();
+}
+
+function scrollNav(){
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', function(e){
+            e.preventDefault(); //Prevenimos que se mueva rapido a la seccion
+
+            // Configuramos el comportamiento
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({behavior: "smooth"});
+        })
+    })
+
 }
 
 function crearGaleria(){
